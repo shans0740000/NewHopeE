c8b723030a75b683e1c4e9cd8b07cc9d987d0649
tianshunjian <tianshunjian@tianshunjiandeMac-mini.local>
2015-05-29 13:56:17 +0800

    Initial Commit

---
+++ NewHopeE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewHopeE
+//
+//  Created by tianshunjian on 15/5/29.
+//  Copyright (c) 2015年 newhopegroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewHopeE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewHopeE
+//
+//  Created by tianshunjian on 15/5/29.
+//  Copyright (c) 2015年 newhopegroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
